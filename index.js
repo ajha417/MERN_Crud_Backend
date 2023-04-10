5ef2e6c46b1af46bc4a4f30804694b0f39f5360f
ajha417 <ajha417@rku.ac.in>
2023-04-10 23:57:12 +0530

    adds routing of update

--- index.js
+++ index.js
@@ -29,7 +29,27 @@ app.get("/read",(req,res)=>{
         res.send(response);
     })
 })
+app.put("/update",async(req,res)=>{
+    const newFoodName = req.body.foodName;
+    const newFoodDesc = req.body.foodDesc;
+    const newItemNum = req.body.itemNum;
+    const id = req.params.id;
+    try{
+      await FoodModel.findById(id,(err,updatedFood)=>{
+        if(err){
+            console.log(err)
+        }
+            updatedFood.foodName = newFoodName;
+            updatedFood.foodDesc = newFoodDesc;
+            updatedFood.itemNum = newItemNum;
+            updatedFood.save();
+            res.send("update");
+        })
+    }catch(err){
 
+    }
+
+})
 app.listen(3001,()=>{
     console.log(`app listening on port 3001`);
 })
