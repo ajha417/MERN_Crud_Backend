d9a818217eb271876d8059a06a4793d7d155efcc
ajha417 <ajha417@rku.ac.in>
2023-04-10 19:08:52 +0530

    inserts data manually using model

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const FoodModel = require('./models/Food.js')
 const app = express();
 
 app.use(express.json());
@@ -8,8 +9,14 @@ mongoose.connect("mongodb://localhost:27017/crudoperation",{
     useNewUrlParser:true
 })
 
-app.get("/",(req,res)=>{
-    
+app.get("/",async (req,res)=>{
+    const food = new FoodModel({
+        foodName:"Burger",
+        foodDesc:"Very tasty",
+        itemNum:5
+    });
+    await food.save();
+    res.send('data inserted successfully');
 })
 
 app.listen(3001,()=>{
