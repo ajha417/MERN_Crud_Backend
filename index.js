b6b633ce39336cdffe231fbc2486a4e5761b5159
ajha417 <ajha417@rku.ac.in>
2023-04-10 22:27:28 +0530

    adds route of insert and read

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const FoodModel = require('./models/Food.js')
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect("mongodb://localhost:27017/crudoperation",{
     useNewUrlParser:true
 })
 
-app.get("/",async (req,res)=>{
+app.post("/insert",async (req,res)=>{
+    const foodName = req.body.foodName;
+    const foodDesc = req.body.foodDesc;
+    const itemNum = req.body.itemNum;
     const food = new FoodModel({
-        foodName:"Burger",
-        foodDesc:"Very tasty",
-        itemNum:5
+        foodName:foodName,
+        foodDesc:foodDesc,
+        itemNum:itemNum
     });
     await food.save();
     res.send('data inserted successfully');
+})
+
+app.get("/read",(req,res)=>{
+    FoodModel.find({}).then(response=>{
+        res.send(response);
+    })
 })
 
 app.listen(3001,()=>{
